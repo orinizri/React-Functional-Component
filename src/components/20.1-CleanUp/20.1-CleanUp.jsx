e3c4b01336121210a42436c3a65517287cb3aa50
orini <ori.nizri.tal@gmail.com>
2021-12-28 13:25:21 +0200

    20.1

--- src/components/20.1-CleanUp/20.1-CleanUp.jsx
+++ src/components/20.1-CleanUp/20.1-CleanUp.jsx
@@ -1,42 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-export const AnotherComponent = (props) => {
-    const manageToggle = () => {
-        if (props.data === true) {
-            return (
-                <div>
-                    {props.sendCountry}
-                </div>
-            )
-        } else {
-            return <div>off</div>
+export const AnotherComponent = () => {
+    const [ data , setData ] = useState('')
+    useEffect(()=>{
+        const fetchData = async () => {
+            const response = await axios.get('https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/');
+            setData(response.data[0].name.common);
         }
-    }
+        fetchData()
+        return (()=>{
+            const CancelToken = axios.CancelToken;
+            const source = CancelToken.source();
+            source.cancel('Operation canceled by the user.');
+        })
+    }, [])
     return (
         <div>
-            {manageToggle()}
+            hello
+            {data}
         </div>
     )
 }
 export const CleanUp = () => {
-        const [ data , setData ] = useState('')
-        const [ toggler, SetToggler ] = useState(true)
-        useEffect(()=>{
-            const fetchData = async () => {
-                const response = await axios.get('https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/');
-                setData(response.data[0].name.common);
-            }
-            fetchData()
-        }, [])
-        const showData = ()=> {
-            if (data) {
-                return <div>{data}</div>
-            }
-        }
+        const [ toggler, SetToggler ] = useState(false)
+
         return (
             <div>
-                <AnotherComponent data={toggler} sendCountry={data}/>
                 <button onClick={()=>SetToggler(!toggler)}>click</button>
+                {toggler && <AnotherComponent/>}
             </div>
     )
 }
